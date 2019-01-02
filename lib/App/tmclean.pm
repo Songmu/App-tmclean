c26b7f66553cf78f710d5f426ad1554cece68e71
Songmu <y.songmu@gmail.com>
2019-01-03 03:31:24 +0900

    Checking in changes prior to tagging of version v0.0.2.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 442c2f2..78f67ef 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension App-tmclean
    
     {{$NEXT}}
    +
    +v0.0.2 2019-01-02T18:31:11Z
         - fix log prefix typo
         - drop Hash::Rename dependency

--- lib/App/tmclean.pm
+++ lib/App/tmclean.pm
@@ -2,7 +2,7 @@ package App::tmclean;
 use 5.010;
 use warnings;
 
-use version 0.77; our $VERSION = version->declare("v0.0.1");
+use version 0.77; our $VERSION = version->declare("v0.0.2");
 
 use Getopt::Long qw/GetOptions :config posix_default no_ignore_case bundling auto_help/;
 use Pod::Usage qw/pod2usage/;
